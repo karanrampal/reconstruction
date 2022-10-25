7b3936232dfe78e1e422388305fdefc3a8e62b52
Karan Rampal <karan.rampal@hm.com>
2022-10-25 16:47:57 +0200

    Fix lint errors

--- reconstruction/datahandler/datahandler.py
+++ reconstruction/datahandler/datahandler.py
@@ -2,12 +2,10 @@
 
 import json
 import os
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
 import open3d as o3d
-
-DataType = Union[float, List[List[float]], Dict[str, float]]
 
 
 class DataHandler:
@@ -17,7 +15,14 @@ class DataHandler:
         img_path: Relative path of images i.e. RGB and depth
         calib_path: Relative path of calibration data
     """
-    def __init__(self, root: str, img_path: str, calib_path: str, cams_to_keep: Optional[List[str]] = None) -> None:
+
+    def __init__(
+        self,
+        root: str,
+        img_path: str,
+        calib_path: str,
+        cams_to_keep: Optional[List[str]] = None,
+    ) -> None:
         self.root = root
         self.img_path = os.path.join(root, img_path)
         self.calibration_path = os.path.join(root, calib_path)
@@ -27,34 +32,43 @@ class DataHandler:
         """Get serial id from file name"""
         return name.split("_")[-1].split(".")[0]
 
-    def _filter_serial(self, data: Dict[str, DataType]):
+    def _filter_serial(self, data: Dict[str, Any]) -> Dict[str, Any]:
         """Filter the camera serial id to keep"""
         if self.cams_to_keep:
-            return {serial: val for serial, val in data.items() if serial in self.cams_to_keep}
+            return {
+                serial: val
+                for serial, val in data.items()
+                if serial in self.cams_to_keep
+            }
         return data
 
-    def _read_json(self, path: str) -> Dict[str, DataType]:
+    def _read_json(self, path: str) -> Dict[str, Any]:
         """Read json file"""
         with open(path, "r", encoding="utf-8") as fout:
             data = json.load(fout)
 
         return self._filter_serial(data)
-    
-    def _load_imgs(self, name: str, type: str) -> Dict[str, o3d.geometry.Image]:
+
+    def _load_imgs(self, name: str, mode: str) -> Dict[str, o3d.geometry.Image]:
         """Load images helper function
         Args:
             name: Name of person
-            type: RGB or depth
+            mode: RGB or depth
         Returns:
             Mapping of camera serial id and image
         """
-        path = os.path.join(self.img_path, name, type)
+        path = os.path.join(self.img_path, name, mode)
         paths = [os.path.join(path, fin) for fin in os.listdir(path)]
-        imgs = {self._get_serial(img_path): o3d.io.read_image(img_path) for img_path in paths}
+        imgs = {
+            self._get_serial(img_path): o3d.io.read_image(img_path)
+            for img_path in paths
+        }
 
         return self._filter_serial(imgs)
-    
-    def load_images(self, name: str) -> Tuple[Dict[str, o3d.geometry.Image], Dict[str, o3d.geometry.Image]]:
+
+    def load_images(
+        self, name: str
+    ) -> Tuple[Dict[str, o3d.geometry.Image], Dict[str, o3d.geometry.Image]]:
         """Load images
         Args:
             name: Name of person
@@ -68,25 +82,29 @@ class DataHandler:
 
     def load_depth_scales(self) -> Dict[str, float]:
         """Load depth scales for each camera"""
-        data = self._read_json(os.path.join(self.calibration_path, "device_depth_scales.json"))
+        data = self._read_json(
+            os.path.join(self.calibration_path, "device_depth_scales.json")
+        )
 
         return data
 
     def load_transformations(self) -> Dict[str, np.ndarray]:
         """Load transformations for each camera exceptht the main camera"""
-        data = self._read_json(os.path.join(self.calibration_path, "transformations.json"))
+        data = self._read_json(
+            os.path.join(self.calibration_path, "transformations.json")
+        )
         out = {serial: np.asarray(mat) for serial, mat in data.items()}
 
         return out
 
     def load_intrinsics(self) -> Dict[str, o3d.camera.PinholeCameraIntrinsic]:
         """Load intrinsics for each camera"""
-        intrin_files = {
+        tmp = {
             f.split("_")[0]: os.path.join(self.calibration_path, f)
             for f in os.listdir(self.calibration_path)
             if "intrinsics.json" in f
         }
-        intrin_files = self._filter_serial(intrin_files)
+        intrin_files = self._filter_serial(tmp)
         out = {}
         for serial, path in intrin_files.items():
             data = self._read_json(path)
