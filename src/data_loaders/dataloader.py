Fix image normalization bug

src/data_loaders/dataloader.py
@@ -30,22 +30,21 @@ class BackPredictionDataset(Dataset):
         self.root = root
         self.file_list = pd.read_csv(os.path.join(root, file_name), header=None)
         self.transforms = transforms
-        self.front_img = np.load(os.path.join(root, "avg_front_image.npy"))
 
-    def _load_img(self, path_: str, avg_img: np.ndarray) -> torch.Tensor:
+    def _load_img(self, path_: str) -> torch.Tensor:
         """Load image and normalzie"""
         img = np.asarray(Image.open(path_))
         img_tensor = torch.tensor(img, dtype=torch.float32)
 
-        result = ((img_tensor - avg_img.min()) / (avg_img.max() - avg_img.min())).unsqueeze(0)
+        result = ((img_tensor - img.min()) / (img.max() - img.min())).unsqueeze(0)
 
         return result
 
-    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """Get an item from the dataset given the index idx"""
         im_name = self.file_list.iloc[idx][0]
-        front_img = self._load_img(os.path.join(self.root, "front", im_name), self.front_img)
-        back_img = self._load_img(os.path.join(self.root, "back", im_name), self.front_img)
+        front_img = self._load_img(os.path.join(self.root, "front", im_name))
+        back_img = self._load_img(os.path.join(self.root, "back", im_name))
 
         front_img = tvt.functional.affine(
             front_img, shear=0.0, scale=2.0, translate=(0, 0), angle=0.0
@@ -58,9 +57,7 @@ class BackPredictionDataset(Dataset):
             front_img = self.transforms(front_img)
             back_img = self.transforms(back_img)
 
-        mask = torch.zeros_like(back_img)
-        mask[back_img > 0.0] = 1.0
-        return front_img, back_img, mask
+        return front_img, back_img
 
     def __len__(self) -> int:
         """Length of the dataset"""