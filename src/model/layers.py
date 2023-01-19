a2e55a2329619f9fcf02b0a767d7f6d158822e71
Karan Rampal <karan.rampal@hm.com>
2023-01-19 12:10:10 +0100

    Refactor layers

--- src/model/layers.py
+++ src/model/layers.py
@@ -1,75 +1,155 @@
 """Utility for neural network layers"""
 
+from typing import Dict, List
+
 import torch
+from torchvision.models.feature_extraction import create_feature_extractor
 
 
-def conv_bn_relu(
-    in_filter: int, out_filter: int, kernel: int, stride: int, pad: int
-) -> torch.nn.Sequential:
-    """Helper function for creating down convolution layer"""
-    return torch.nn.Sequential(
-        torch.nn.Conv2d(in_filter, out_filter, kernel_size=kernel, stride=stride, padding=pad),
-        torch.nn.BatchNorm2d(out_filter),
-        torch.nn.ReLU(),
+def get_activation(activation_name: str) -> torch.nn.Module:
+    """Get relevant activation module
+    Args:
+        activation_name: Activation layer name, can be relu, leaky, sigmoid or identity
+    """
+    activation_dict = torch.nn.ModuleDict(
+        {
+            "relu": torch.nn.ReLU(),
+            "leaky": torch.nn.LeakyReLU(0.02),
+            "sigmoid": torch.nn.Sigmoid(),
+            "identity": torch.nn.Identity(),
+        }
     )
+    return activation_dict[activation_name]
 
 
-def trans_conv_bn_relu(
-    in_filter: int, out_filter: int, kernel: int, stride: int, pad: int
-) -> torch.nn.Sequential:
-    """Helper function for creating up convolution layer"""
-    return torch.nn.Sequential(
-        torch.nn.ConvTranspose2d(
-            in_filter,
-            out_filter,
-            kernel_size=kernel,
-            stride=stride,
-            padding=pad,
-            output_padding=1,
-        ),
-        torch.nn.BatchNorm2d(out_filter),
-        torch.nn.ReLU(),
+def get_normalization(normalization_name: str, out_filter: int) -> torch.nn.Module:
+    """Get relevant normalization module
+    Args:
+        normalization_name: Normalization layer, can be batch or identity
+        out_filter: Number of output filters in the preceding conv layer
+    """
+    normalization_dict = torch.nn.ModuleDict(
+        {
+            "batch": torch.nn.BatchNorm2d(out_filter),
+            "identity": torch.nn.Identity(),
+        }
     )
+    return normalization_dict[normalization_name]
 
 
-def trans_conv_relu(
-    in_filter: int, out_filter: int, kernel: int, stride: int, pad: int
-) -> torch.nn.Sequential:
-    """Helper function for creating up convolution layer"""
-    return torch.nn.Sequential(
-        torch.nn.ConvTranspose2d(
-            in_filter,
-            out_filter,
-            kernel_size=kernel,
-            stride=stride,
-            padding=pad,
-            output_padding=1,
-        ),
-        torch.nn.ReLU(),
-    )
+class ConvBnActLayer(torch.nn.Module):
+    """Custom convolution, batch norm, activation layer
+    Args:
+        normalization_name: Normalization layer, can be batch or identity
+        activation_name: Activation layer name, can be relu, leaky, sigmoid or identity
+    Kwargs:
+        in_filter: Number of input filters
+        out_filter: Number of output filters
+        kernel: Kernel size
+        stride: Convolution stride length
+        pad: Convolution input padding
+    """
+
+    def __init__(self, normalization_name: str, activation_name: str, **kwargs: int) -> None:
+        super().__init__()
+        self.conv = torch.nn.Conv2d(
+            kwargs["in_filter"],
+            kwargs["out_filter"],
+            kernel_size=kwargs["kernel"],
+            stride=kwargs["stride"],
+            padding=kwargs["pad"],
+        )
+        self.batch_norm = get_normalization(normalization_name, kwargs["out_filter"])
+        self.activation = get_activation(activation_name)
+
+    def forward(self, x_inp: torch.Tensor) -> torch.Tensor:
+        """Forward propagation"""
+        out = self.conv(x_inp)
+        out = self.batch_norm(out)
+        return self.activation(out)
 
 
-def trans_conv_sigmoid(
-    in_filter: int, out_filter: int, kernel: int, stride: int, pad: int
-) -> torch.nn.Sequential:
-    """Helper function for creating up convolution layer"""
-    return torch.nn.Sequential(
-        torch.nn.ConvTranspose2d(
-            in_filter,
-            out_filter,
-            kernel_size=kernel,
-            stride=stride,
-            padding=pad,
-            output_padding=1,
-        ),
-        torch.nn.Sigmoid(),
-    )
+class TransConvBnActLayer(torch.nn.Module):
+    """Custom transposed convolution, batch norm, activation layer
+    Args:
+        normalization_name: Normalization layer, can be batch or identity
+        activation_name: Activation layer name, can be relu, leaky, sigmoid or identity
+    Kwargs:
+        in_filter: Number of input filters
+        out_filter: Number of output filters
+        kernel: Kernel size
+        stride: Transposed convolution stride length
+        pad: Transposed convolution input padding
+        out_pad: Transposed convolution output padding
+    """
+
+    def __init__(self, normalization_name: str, activation_name: str, **kwargs: int) -> None:
+        super().__init__()
+        self.trans_conv = torch.nn.ConvTranspose2d(
+            kwargs["in_filter"],
+            kwargs["out_filter"],
+            kernel_size=kwargs["kernel"],
+            stride=kwargs["stride"],
+            padding=kwargs["pad"],
+            output_padding=kwargs["out_pad"],
+        )
+        self.batch_norm = get_normalization(normalization_name, kwargs["out_filter"])
+        self.activation = get_activation(activation_name)
+
+    def forward(self, x_inp: torch.Tensor) -> torch.Tensor:
+        """Forward propagation"""
+        out = self.trans_conv(x_inp)
+        out = self.batch_norm(out)
+        return self.activation(out)
 
 
-def linear_bn_relu(in_: int, out_: int) -> torch.nn.Sequential:
-    """Linear layers helper function"""
-    return torch.nn.Sequential(
-        torch.nn.Linear(in_, out_),
-        torch.nn.BatchNorm1d(out_),
-        torch.nn.ReLU(),
-    )
+class UpConvBnActLayer(torch.nn.Module):
+    """Custom upsampling, convolution, batch norm, activation layer
+    Args:
+        scale: Upsampling scale factor
+        norm_name: Normalization layer, can be batch or identity
+        activation_name: Activation layer name, can be relu, leaky, sigmoid or identity
+    Kwargs:
+        in_filter: Number of input filters
+        out_filter: Number of output filters
+        kernel: Kernel size
+        stride: Convolution stride length
+        pad: Convolution input padding
+    """
+
+    def __init__(self, scale: float, norm_name: str, activation_name: str, **kwargs: int) -> None:
+        super().__init__()
+        self.upsample = torch.nn.Upsample(scale_factor=scale)
+        self.conv = torch.nn.Conv2d(
+            kwargs["in_filter"],
+            kwargs["out_filter"],
+            kernel_size=kwargs["kernel"],
+            stride=kwargs["stride"],
+            padding=kwargs["pad"],
+        )
+        self.batch_norm = get_normalization(norm_name, kwargs["out_filter"])
+        self.activation = get_activation(activation_name)
+
+    def forward(self, x_inp: torch.Tensor) -> torch.Tensor:
+        """Forward propagation"""
+        out = self.upsample(x_inp)
+        out = self.conv(out)
+        out = self.batch_norm(out)
+        return self.activation(out)
+
+
+class ReconstructionModel(torch.nn.Module):
+    """Prediction model of back depth map
+    Args:
+        base_model: Base model to extend
+        nodes: List of nodes in the output
+    """
+
+    def __init__(self, base_model: torch.nn.Module, nodes: List[str]) -> None:
+        super().__init__()
+        self.base_model = base_model
+        self.model = create_feature_extractor(base_model, return_nodes=nodes)
+
+    def forward(self, x_inp: torch.Tensor) -> Dict[str, torch.Tensor]:
+        """Forward propagation"""
+        return self.model(x_inp)
--- src/model/net.py
+++ src/model/net.py
@@ -1,9 +1,8 @@
 """Neural Network model"""
 
-from typing import Dict, List
+from typing import List
 
 import torch
-from torchvision.models.feature_extraction import create_feature_extractor
 
 import model.layers as layer
 
@@ -16,24 +15,24 @@ class BPModel(torch.nn.Module):
         embeddings: Embedding dimension
     """
 
-    def __init__(self, filters: List[int], kernels: List[int], embeddings: int) -> None:
+    def __init__(self, filters: List[int], kernels: List[int]) -> None:
         super().__init__()
         filters = [1] + filters
-
-        # Encoder
         self.encoder = self._encoder(filters, kernels)
-
-        # Linear
-        self.fc1 = layer.linear_bn_relu(4 * 4 * filters[-1], embeddings)
-        self.fc2 = layer.linear_bn_relu(embeddings, 4 * 4 * filters[-1])
-
-        # Decoder
         self.decoder = self._decoder(filters, kernels)
 
     def _encoder(self, filters: List[int], kernels: List[int]) -> torch.nn.Sequential:
         """Encoder helper function"""
         layers = [
-            layer.conv_bn_relu(filters[i], filters[i + 1], kernels[i], 2, kernels[i] // 2)
+            layer.ConvBnActLayer(
+                "batch",
+                "relu",
+                in_filter=filters[i],
+                out_filter=filters[i + 1],
+                kernel=kernels[i],
+                stride=2,
+                pad=kernels[i] // 2,
+            )
             for i in range(len(kernels))
         ]
         return torch.nn.Sequential(*layers)
@@ -41,45 +40,45 @@ class BPModel(torch.nn.Module):
     def _decoder(self, filters: List[int], kernels: List[int]) -> torch.nn.Sequential:
         """Decoder helper function"""
         layers = [
-            layer.trans_conv_bn_relu(filters[i + 1], filters[i], kernels[i], 2, kernels[i] // 2)
+            layer.TransConvBnActLayer(
+                "batch",
+                "relu",
+                in_filter=filters[i + 1],
+                out_filter=filters[i],
+                kernel=kernels[i],
+                stride=2,
+                pad=kernels[i] // 2,
+                out_pad=1,
+            )
             for i in range(len(kernels) - 1, 1, -1)
         ]
         layers += [
-            layer.trans_conv_relu(filters[2], filters[1], kernels[1], 2, kernels[1] // 2),
-            layer.trans_conv_sigmoid(filters[1], filters[0], kernels[0], 2, kernels[0] // 2),
+            layer.TransConvBnActLayer(
+                "identity",
+                "relu",
+                in_filter=filters[2],
+                out_filter=filters[1],
+                kernel=kernels[1],
+                stride=2,
+                pad=kernels[1] // 2,
+                out_pad=1,
+            ),
+            layer.TransConvBnActLayer(
+                "identity",
+                "relu",
+                in_filter=filters[1],
+                out_filter=filters[0],
+                kernel=kernels[0],
+                stride=2,
+                pad=kernels[0] // 2,
+                out_pad=1,
+            ),
         ]
         return torch.nn.Sequential(*layers)
 
     def forward(self, x_inp: torch.Tensor) -> torch.Tensor:
         """Forward propagation"""
         x_inp = self.encoder(x_inp)
-        x_inp = x_inp.reshape(-1, 4 * 4 * 256)
-
-        x_inp = self.fc1(x_inp)
-        x_inp = self.fc2(x_inp)
-
-        x_inp = x_inp.reshape(-1, 256, 4, 4)
         out = self.decoder(x_inp)
 
         return out
-
-
-class ReconstructionModel(torch.nn.Module):
-    """Prediction model of back depth map
-    Args:
-        filters: List of filters in each encoder layer
-        kernels: List of kernels in each encoder layer
-        embeddings: Embedding dimension
-        nodes: List of nodes in the output
-    """
-
-    def __init__(
-        self, filters: List[int], kernels: List[int], embeddings: int, nodes: List[str]
-    ) -> None:
-        super().__init__()
-        model = BPModel(filters, kernels, embeddings)
-        self.model = create_feature_extractor(model, return_nodes=nodes)
-
-    def forward(self, x_inp: torch.Tensor) -> Dict[str, torch.Tensor]:
-        """Forward propagation"""
-        return self.model(x_inp)
