55ddfbfd84160038b6b2b05a46130b84ded7d369
Karan Rampal <karan.rampal@hm.com>
2023-03-14 13:13:25 +0100

    Add vgg loss

--- src/model/losses.py
+++ src/model/losses.py
@@ -1,13 +1,13 @@
 """Loss functions"""
 
-from typing import Dict
+from typing import Dict, Tuple
 
 import torch
 
 from config_manager.manager import Params
 
-MSE_LOSS = torch.nn.MSELoss()
 L1_LOSS = torch.nn.L1Loss()
+CE_LOSS = torch.nn.CrossEntropyLoss()
 
 
 def total_variation_loss(outputs: torch.Tensor) -> torch.Tensor:
@@ -32,31 +32,176 @@ def style_loss(outputs: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
     """
     out_loss = torch.einsum("bcij,bdij->bcd", outputs, outputs)
     label_loss = torch.einsum("bcij,bdij->bcd", labels, labels)
-    return (out_loss - label_loss).abs().mean()
+    return (out_loss - label_loss).abs().mean() / (outputs.shape[-1] * outputs.shape[-2])
+
+
+def log_l1_loss(outputs: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    """Calculate the log L1 loss
+    Args:
+        outputs: Model predictions
+        labels: Ground truth labels
+    Returns:
+        Log L1 loss
+    """
+    return torch.log(1.0 + (outputs - labels).abs()).mean()
+
+
+def grad_loss(
+    outputs: torch.Tensor, labels: torch.Tensor, mask: torch.Tensor, device: str
+) -> torch.Tensor:
+    """Gradient loss
+    Args:
+        outputs: Model predictions
+        labels: Ground truth labels
+        device: CPU or GPU
+    Returns:
+        Gradient loss
+    """
+    sobel = torch.tensor(
+        [
+            [[[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]]],
+            [[[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]]],
+        ],
+        device=torch.device(device),
+    )
+
+    out = torch.nn.functional.conv2d(outputs, sobel, padding="same")
+    g_t = torch.nn.functional.conv2d(labels, sobel, padding="same")
+
+    return torch.log(
+        (out[mask.expand(-1, 2, -1, -1)] - g_t[mask.expand(-1, 2, -1, -1)]).abs() + 1.0
+    ).mean()
+
+
+def discriminator_loss(
+    disc_emb: torch.Tensor, net_emb: torch.Tensor, params: Params
+) -> Tuple[torch.Tensor, float, float]:
+    """Discriminator loss
+    Args:
+        disc_emb: Discrimindator embeddings
+        net_emb: Net embeddings
+        params: Hyper-parameters
+    Returns:
+        Discriminator loss
+    """
+    emb = net_emb.flatten(start_dim=1)
+    emb = emb / emb.norm(dim=1, keepdim=True)
+    inp = disc_emb.flatten(start_dim=1)
+    inp = inp / inp.norm(dim=1, keepdim=True)
+
+    out = (inp @ emb.t()) * torch.exp(torch.tensor(params.temperature))
+
+    batch_size = out.shape[0]
+    labels = torch.arange(batch_size, device=torch.device(params.device))
+    tmp = CE_LOSS(out, labels) + CE_LOSS(out.t(), labels)
+
+    acc = avg_acc_gpu(out.detach(), labels.detach())
+    f_1 = avg_f1_score_gpu(out.detach(), labels.detach(), batch_size)
+
+    return tmp / 2.0, acc, f_1
 
 
 def loss_fn(
     outputs: Dict[str, torch.Tensor],
-    out_label: Dict[str, torch.Tensor],
+    vgg_outputs: Dict[str, torch.Tensor],
+    label_out: Dict[str, torch.Tensor],
     labels: torch.Tensor,
+    mask: torch.Tensor,
     params: Params,
-) -> torch.Tensor:
+) -> Tuple[torch.Tensor, float, float]:
     """Loss function
     Args:
         outputs: Model predictions
-        out_label: Model prediction on the ground truth
+        vgg_outputs: Ground truth model outputs
         labels: Ground truth labels
         params: Hyperparameters
     Returns:
         Total loss
     """
     predictions = outputs[params.output_layer]
+    num = len(params.style_layers)
+    num_cascades = len(params.cascade_layers) + 1
 
-    loss = MSE_LOSS(predictions, labels) * params.mse
+    loss = L1_LOSS(predictions[mask], labels[mask]) * params.person
+    for cascade_names in params.cascade_layers + [params.output_layer]:
+        out = outputs[cascade_names]
+        loss += L1_LOSS(out[~mask], labels[~mask]) * params.bg
+        loss += log_l1_loss(out[mask], labels[mask]) * params.exp
+        loss += grad_loss(out, labels, mask, params.device) * params.grad
+
     loss += total_variation_loss(predictions) * params.tvl
-    for node in params.style_nodes:
-        loss += style_loss(outputs[node], out_label[node]) * params.style
-        loss += L1_LOSS(outputs[node], out_label[node]) * params.content
 
-    loss /= params.mse + params.tvl + params.style + params.content
-    return loss
+    for node, node_v in zip(params.style_layers, params.vgg_style):
+        loss += style_loss(outputs[node], vgg_outputs[node_v]) * params.style
+        loss += L1_LOSS(outputs[node], vgg_outputs[node_v]) * params.perceptual
+
+    tmp, acc, f_1 = discriminator_loss(
+        label_out[params.embedding_layer], outputs[params.embedding_layer], params
+    )
+    loss += tmp * params.disc
+
+    loss /= (
+        (params.person + params.bg + params.grad + params.exp) * num_cascades
+        + (params.style + params.perceptual) * num
+        + params.tvl
+        + params.disc
+    )
+    return loss, acc, f_1
+
+
+def avg_acc_gpu(outputs: torch.Tensor, labels: torch.Tensor) -> float:
+    """Compute the accuracy, given the outputs and labels for all images.
+    Args:
+        outputs: Logits of the network
+        labels: Ground truth labels
+    Returns:
+        average accuracy in [0,1]
+    """
+    preds = outputs.argmax(dim=1).to(torch.int64)
+    avg_acc = (preds == labels).to(torch.float32).mean()
+    return avg_acc.item()
+
+
+def avg_f1_score_gpu(
+    outputs: torch.Tensor, labels: torch.Tensor, num_classes: int, eps: float = 1e-7
+) -> float:
+    """Compute the F1 score, given the outputs and labels for all images.
+    Args:
+        outputs: Logits of the network
+        labels: Ground truth labels
+        num_classes: Number of classes
+        eps: Epsilon
+    Returns:
+        average f1 score
+    """
+    preds = (outputs).argmax(dim=1).to(torch.int64)
+    pred_ohe = torch.nn.functional.one_hot(preds, num_classes)
+    label_ohe = torch.nn.functional.one_hot(labels, num_classes)
+
+    true_pos = (label_ohe * pred_ohe).sum(0)
+    false_pos = ((1 - label_ohe) * pred_ohe).sum(0)
+    false_neg = (label_ohe * (1 - pred_ohe)).sum(0)
+
+    precision = true_pos / (true_pos + false_pos + eps)
+    recall = true_pos / (true_pos + false_neg + eps)
+    avg_f1 = 2 * (precision * recall) / (precision + recall + eps)
+    wts = label_ohe.sum(0)
+    wtd_macro_f1 = (avg_f1 * wts).sum() / wts.sum()
+
+    return wtd_macro_f1.item()
+
+
+def confusion_matrix(outputs: torch.Tensor, labels: torch.Tensor, num_classes: int) -> torch.Tensor:
+    """Create confusion matrix
+    Args:
+        outputs: Logits of the network
+        labels: Ground truth labels
+        num_classes: Number of classes
+    Returns:
+        Confusion matrix as a tensor
+    """
+    num = labels.shape[0]
+    conf_mat = torch.zeros((1, num, num_classes, num_classes), dtype=torch.int64)
+    preds = (outputs).argmax(dim=1).to(torch.int64)
+    conf_mat[0, range(num), labels, preds] = 1
+    return conf_mat.sum(1, keepdim=True)
--- src/trainers/train_engine.py
+++ src/trainers/train_engine.py
@@ -1,27 +1,33 @@
 """Training and evaluation functions"""
 
 import os
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple
 
-import numpy as np
 import torch
+import torchvision.transforms as tvt
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
+from torchvision.models import VGG19_BN_Weights, vgg19_bn
+from torchvision.models.feature_extraction import create_feature_extractor
+from tqdm.notebook import tqdm
 
 from config_manager.manager import Params
 
 
 def train_per_epoch(
     net: torch.nn.Module,
+    vgg: torch.nn.Module,
+    preprocess: tvt._presets.ImageClassification,
     train_dataloader: DataLoader,
     criterion: torch.nn.modules.loss._Loss,
     optimizer: torch.optim.Optimizer,
     params: Params,
-) -> float:
+) -> Tuple[float, float, float, float]:
     """Train for an epoch
     Args:
         net: Neural network
+        vgg: VGG network
+        preprocess: VGG pre-processor
         train_dataloader: Training dataloader
         criterion: Loss function
         optimizer: Optimizer
@@ -31,31 +37,44 @@ def train_per_epoch(
     """
     net.train()
 
-    epoch_loss = []
-    for inputs, labels in tqdm(train_dataloader):
-        inputs = inputs.to(params.device)
-        labels = labels.to(params.device)
+    epoch_loss, accuracy, f1_score, err = 0.0, 0.0, 0.0, 0.0
+    for inputs, labels, mask in tqdm(train_dataloader):
+        inputs = inputs.to(torch.device(params.device))
+        labels = labels.to(torch.device(params.device))
+        mask = mask.to(torch.device(params.device))
 
         optimizer.zero_grad()
-        outputs = net(inputs)
-        label_outputs = net(labels)
-        loss = criterion(outputs, label_outputs, labels, params)
+        outputs = net(inputs, mask.to(torch.float32))
+        label_out = net(labels, mask.to(torch.float32))
+        with torch.no_grad():
+            tmp = preprocess(labels.expand(-1, 3, -1, -1))
+            vgg_outputs = vgg(tmp)
+        loss, acc, f_1 = criterion(outputs, vgg_outputs, label_out, labels, mask, params)
         loss.backward()
         optimizer.step()
 
-        epoch_loss.append(loss.item())
-    return np.mean(epoch_loss)
+        epoch_loss += loss.item()
+        accuracy += acc
+        f1_score += f_1
+        err += (outputs[params.output_layer].detach() - labels.detach()).abs().mean()
+
+    num = len(train_dataloader)
+    return epoch_loss / num, accuracy / num, f1_score / num, err / num
 
 
 def evaluate(
     net: torch.nn.Module,
+    vgg: torch.nn.Module,
+    preprocess: tvt._presets.ImageClassification,
     test_dataloader: DataLoader,
     criterion: torch.nn.modules.loss._Loss,
     params: Params,
-) -> Tuple[torch.Tensor, float]:
+) -> Tuple[torch.Tensor, float, float, float, float]:
     """Evaluate modelh
     Args:
         net: Neural network
+        vgg: VGG network
+        preprocess: VGG pre-processor
         test_dataloader: Test dataloader
         criterion: Loss function
         params: Hyperparameters
@@ -64,32 +83,63 @@ def evaluate(
     """
     net.eval()
 
-    epoch_loss = []
+    epoch_loss, accuracy, f1_score, err = 0.0, 0.0, 0.0, 0.0
     with torch.no_grad():
-        for images, labels in test_dataloader:
-            images = images.to(params.device)
-            labels = labels.to(params.device)
+        for images, labels, mask in test_dataloader:
+            images = images.to(torch.device(params.device))
+            labels = labels.to(torch.device(params.device))
+            mask = mask.to(torch.device(params.device))
 
-            outputs = net(images)
-            label_outputs = net(labels)
-            loss = criterion(outputs, label_outputs, labels, params)
+            outputs = net(images, mask.to(torch.float32))
+            label_out = net(labels, mask.to(torch.float32))
+            tmp = preprocess(labels.expand(-1, 3, -1, -1))
+            vgg_outputs = vgg(tmp)
+            loss, acc, f_1 = criterion(outputs, vgg_outputs, label_out, labels, mask, params)
 
-            epoch_loss.append(loss.item())
-    return outputs[params.output_layer], np.mean(epoch_loss)
+            epoch_loss += loss.item()
+            accuracy += acc
+            f1_score += f_1
+            err += (outputs[params.output_layer] - labels).abs().mean()
+
+    num = len(test_dataloader)
+    return outputs[params.output_layer], epoch_loss / num, accuracy / num, f1_score / num, err / num
 
 
-# pylint: disable=too-many-arguments
+def get_style_net(params: Params) -> Tuple[torch.nn.Module, tvt._presets.ImageClassification]:
+    """Get network for comparing styles
+    Args:
+        params: Hyper-parameters
+    Returns:
+        Style neural network and it's pre-processor
+    """
+    weights = VGG19_BN_Weights.IMAGENET1K_V1
+    preprocess = weights.transforms()
+
+    vmodel = vgg19_bn(weights=weights)
+    for param in vmodel.parameters():
+        param.requires_grad = False
+    vmodel.eval()
+
+    vgg = create_feature_extractor(vmodel, return_nodes=params.vgg_style)
+    vgg.to(torch.device(params.device))
+
+    return vgg, preprocess
+
+
 def train_evaluate(
+    base_model: torch.nn.Module,
     net: torch.nn.Module,
     dataloader: Dict[str, DataLoader],
     criterion: torch.nn.modules.loss._Loss,
     optimizer: torch.optim.Optimizer,
     scheduler: torch.optim.lr_scheduler._LRScheduler,
     params: Params,
-) -> Tuple[List[float], List[float]]:
+) -> None:
     """Train and evaluate model
     Args:
+        base_model: Base model to plot the graph
         net: Neural network
+        discriminator: Discriminator network
         dataloader: Train/test dataloaders
         criterion: Loss function
         optimizer: Optimizer
@@ -98,25 +148,38 @@ def train_evaluate(
     Returns:
         train and test losses
     """
-    writer = SummaryWriter(params.tb_path)
-    images, _ = next(iter(dataloader["train"]))
-    writer.add_graph(net.base_model, images)
+    print("Writing graph ...")
+    writer = SummaryWriter(params.save_path)
+    # images, _, _ = next(iter(dataloader["train"]))
+    # writer.add_graph(base_model, images)
 
-    net = net.to(params.device)
+    net.to(torch.device(params.device))
+    vgg, preprocess = get_style_net(params)
 
     best_loss = 100.0
-    train_loss, test_loss = [], []
+    print("Training started ...")
     for epoch in range(params.epochs):
-        t_loss = train_per_epoch(net, dataloader["train"], criterion, optimizer, params)
-        print(f"Epoch: {epoch + 1}/{params.epochs}, Train loss: {t_loss:.3f}")
+        print(f"\nEpoch: {epoch + 1}/{params.epochs}")
+        t_loss, acc, f1_score, err = train_per_epoch(
+            net, vgg, preprocess, dataloader["train"], criterion, optimizer, params
+        )
+        print(
+            f"Train loss: {t_loss:.3f}, error: {err:.3f}, accuracy: {acc:.3f}, F1: {f1_score:.3f}"
+        )
         writer.add_scalar("Loss/train", t_loss, epoch)
-        train_loss.append(t_loss)
+        writer.add_scalar("accuracy/train", acc, epoch)
+        writer.add_scalar("F1/train", f1_score, epoch)
+        writer.add_scalar("Error/train", err, epoch)
 
-        output, e_loss = evaluate(net, dataloader["test"], criterion, params)
-        print(f"Test loss: {e_loss:.3f}")
+        output, e_loss, acc, f1_score, err = evaluate(
+            net, vgg, preprocess, dataloader["test"], criterion, params
+        )
+        print(f"Test loss: {e_loss:.3f}, error: {err:.3f}, accuracy: {acc:.3f}, F1: {f1_score:.3f}")
         writer.add_scalar("Loss/test", e_loss, epoch)
+        writer.add_scalar("accuracy/test", acc, epoch)
+        writer.add_scalar("F1/test", f1_score, epoch)
+        writer.add_scalar("Error/test", err, epoch)
         writer.add_images("Test batch", output, epoch)
-        test_loss.append(e_loss)
 
         scheduler.step()
         writer.add_scalar("Learning rate", scheduler.get_last_lr()[0], epoch)
@@ -129,5 +192,4 @@ def train_evaluate(
 
     print("Saving last model ...")
     torch.save(net.state_dict(), os.path.join(params.save_path, f"last_{params.epochs}_net.pt"))
-
-    return train_loss, test_loss
+    params.save(os.path.join(params.save_path, "params.yml"))
